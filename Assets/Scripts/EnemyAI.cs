090a3609f119a8c6fdf2b98b887556990ba9700d
Aakash Raghunathan <aakshu.goku@gmail.com>
2020-03-05 20:53:40 +0530

    Attack If Provoked

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,18 +8,58 @@ public class EnemyAI : MonoBehaviour
 {
 
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+    [SerializeField] float distanceToTarget = Mathf.Infinity;
     NavMeshAgent navMeshAgent;
 
+    bool isProvoked = false;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if(distanceToTarget<=chaseRange)
+        {
+            isProvoked = true;
+        }
+
+    }
+
+    private void EngageTarget()
+    {
+        if(distanceToTarget>=navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        else
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
         navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        print("Exterminate!");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
