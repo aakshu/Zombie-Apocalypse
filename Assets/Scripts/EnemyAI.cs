6ce4dd0bf3bc4c5c2a06302d0018d97cd7e4599c
Aakash Raghunathan <aakshu.goku@gmail.com>
2020-03-07 13:12:04 +0530

    Added Placeholder Animations

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -10,14 +10,18 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] Transform target;
     [SerializeField] float chaseRange = 5f;
     [SerializeField] float distanceToTarget = Mathf.Infinity;
+    [SerializeField] float turnSpeed = 5f;
+
     NavMeshAgent navMeshAgent;
 
     bool isProvoked = false;
 
+    Animator enemyAnimator;
 
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        enemyAnimator = GetComponent<Animator>();
     }
 
     void Update()
@@ -37,6 +41,7 @@ public class EnemyAI : MonoBehaviour
 
     private void EngageTarget()
     {
+        FaceTarget();
         if(distanceToTarget>=navMeshAgent.stoppingDistance)
         {
             ChaseTarget();
@@ -49,12 +54,21 @@ public class EnemyAI : MonoBehaviour
 
     private void ChaseTarget()
     {
+        enemyAnimator.SetBool("Attack", false);
+        enemyAnimator.SetTrigger("Move");
         navMeshAgent.SetDestination(target.position);
     }
 
     private void AttackTarget()
     {
-        print("Exterminate!");
+        enemyAnimator.SetBool("Attack", true);
+    }
+
+    private void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
     }
 
     private void OnDrawGizmosSelected()
