b0e14ac9515c82f1fe2b5e172c9ae75cbf4a7ac4
Aakash Raghunathan <aakshu.goku@gmail.com>
2020-03-07 14:08:13 +0530

    Added Game Over Canvas

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -11,7 +11,7 @@ public class PlayerHealth : MonoBehaviour
         hitPoints -= damage;
         if (hitPoints <= 0)
         {
-            print("You dead motherfucker");
+            GetComponent<DeathHandler>().HandleDeath();
         }
     }
 }
