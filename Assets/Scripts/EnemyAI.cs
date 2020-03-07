46a962882acdac5c95ed3d6a42e1a818787887a0
Aakash Raghunathan <aakshu.goku@gmail.com>
2020-03-07 23:09:40 +0530

    Added Weapon Zoom

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -71,6 +71,11 @@ public class EnemyAI : MonoBehaviour
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
     }
 
+    public void OnDamageTaken()
+    {
+        isProvoked = true;
+    }
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomedInFOV = 20f;
+    [SerializeField] float zoomedOutSensitivity = 2f;
+    [SerializeField] float zoomedInSensitivity = .5f;
+
+    bool zoomedInToggle = false;
+    RigidbodyFirstPersonController fpsController;
+    private void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
+    private void Update()
+    {
+        if(zoomedInToggle)
+        {
+            fpsCamera.fieldOfView = zoomedInFOV;
+            fpsController.mouseLook.XSensitivity = zoomedInSensitivity;
+            fpsController.mouseLook.YSensitivity = zoomedInSensitivity;
+        }
+        else
+        {
+            fpsCamera.fieldOfView = zoomedOutFOV;
+            fpsController.mouseLook.XSensitivity = zoomedOutSensitivity;
+            fpsController.mouseLook.YSensitivity = zoomedOutSensitivity;
+        }
+        if(Input.GetMouseButtonDown(1))
+        {
+            zoomedInToggle = !zoomedInToggle;
+        }
+    }
+}
