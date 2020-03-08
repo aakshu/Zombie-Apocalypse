a3eee0e4402da932c22e858f7457666edc6e6807
Aakash Raghunathan <aakshu.goku@gmail.com>
2020-03-08 22:24:18 +0530

    Added Terrain + Underground Dungeon

---
+++ Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    [SerializeField] float lightDecay = 0.1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minimumAngle = 40f;
+
+    Light myLight;
+
+    private void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    public void RestoreLightAngle(float restoreAngle)
+    {
+        myLight.spotAngle = restoreAngle;
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        myLight.intensity -= lightDecay * Time.deltaTime;
+    }
+
+    private void DecreaseLightAngle()
+    {
+        if (myLight.spotAngle <= minimumAngle)
+        {
+            return;
+        }
+        else
+        {
+            myLight.spotAngle -= angleDecay * Time.deltaTime;
+        }       
+    }
+}
